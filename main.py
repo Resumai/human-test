de20fe8eaab386a1719f2119e90024b0f67e6165
Resumai <demannuresu@gmail.com>
2025-04-22 13:48:41 +0300

    Adjusted function, created separate while loop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from zmogus import Zmogus
+from os import system, name
 
-
+def clear():
+    system('cls' if name == 'nt' else 'clear')
 
 def average(numbers):
     return sum(numbers) / len(numbers)
@@ -8,14 +10,26 @@ def average(numbers):
 
 
 def create_person():
-    while True:
-        try:
-            name = input("Enter your name: ")
-            surname = input("Enter your surname: ")
-            age = int(input("Enter your age: "))
-            if age < 0:
-                raise ValueError("Age cannot be negative.")
-            return Zmogus(name, surname, age)
-        except ValueError as e:
-            print(f"Invalid input: {e}. Please try again.")
+    print("--- Enter person's data---")
+    name = input("Enter name: ")
+    surname = input("Enter surname: ")
+    try:
+        age = int(input("Enter age: "))
+        if age < 0:
+            raise ValueError("Age cannot be negative.")
+    except ValueError as e:
+        print(f"Invalid input: {e}. Please try again.")
     
+    return Zmogus(name, surname, age)
+
+
+
+def main():
+    while True:
+        person = create_person()
+        print(person)
+        if input("Do you want to add another person? (y/n): ") != "y":
+            break
+
+
+main()
