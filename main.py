9e7a29be04ec86329f4697cc3a2cc17fdfe5c19a
Resumai <demannuresu@gmail.com>
2025-04-22 11:59:06 +0300

    average function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# initial commit
+def average(list_of_numbers):
+
+    total = 0
+    count = 0 
+
+    for x in list_of_numbers:
+        total += x
+        count += 1
+
+    return total / count
