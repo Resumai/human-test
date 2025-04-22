93d3cc26a5ec4db44e502e97ef992e3167afc675
Resumai <demannuresu@gmail.com>
2025-04-22 14:29:16 +0300

    Adjusted while loop

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from zmogus import Zmogus
 from os import system, name
 
+
+
+# Added 
 def clear():
     system('cls' if name == 'nt' else 'clear')
 
-def average(numbers):
-    return sum(numbers) / len(numbers)
+clear()
 
+
+def average(people : list[Zmogus]):
+    sum_of_ages = 0
+
+    for person in people:
+        sum_of_ages += person.age
+    return sum_of_ages / len(people)
 
 
 def create_person():
@@ -25,11 +34,13 @@ def create_person():
 
 
 def main():
+    people = []
     while True:
+        clear()
         person = create_person()
-        print(person)
+        people.append(person)
+        print(f"Average age: {average(people)}")
         if input("Do you want to add another person? (y/n): ") != "y":
             break
-
 
 main()
